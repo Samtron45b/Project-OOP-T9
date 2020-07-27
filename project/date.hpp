3d8a24a6e8b11fcd3e1a52f343808b4f2f6c75fb
Samtron45b <tien.nnang@gmail.com>
2020-07-27 14:05:49 +0700

    date lib

---
+++ project/date.hpp
@@ -0,0 +1,32 @@
+#ifndef DATE_H_
+#define DATE_H_
+
+#include <iostream>
+
+struct date
+{
+    int year,month,day;
+
+    date();
+    date(int y,int m,int d);
+
+    int maxDay();
+
+    bool leapYear();
+
+    bool operator==(const date& rhs);
+    bool operator!=(const date& rhs);
+    bool operator<=(const date& rhs);
+    bool operator>=(const date& rhs);
+    bool operator<(const date& rhs);
+    bool operator>(const date& rhs);
+    
+    bool birthdayMoth();
+
+    friend std::istream& operator>>(std::istream& in, date& a);
+    friend std::ostream& operator<<(std::ostream& out,const date a);
+    float age(bool);
+};
+
+
+#endif // !date_H_

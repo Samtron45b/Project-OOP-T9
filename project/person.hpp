8298b9c0c1112b7f24bfbd39b7f980c0a9c8e942
Samtron45b <tien.nnang@gmail.com>
2020-07-29 13:00:21 +0700

    adjust person.hpp

--- project/person.hpp
+++ project/person.hpp
@@ -2,7 +2,7 @@
 #define PERSON_H_
 #include <string>
 #include "date.hpp"
-
+#include <vector>
 class person
 {
     private:
@@ -13,11 +13,32 @@ class person
         person();
         virtual ~person();
         virtual void input();
-        virtual void load(std::istream&);
         virtual void output();
         virtual void print(std::ostream&) const;
+        virtual void load(std::istream&);
         friend std::istream& operator>>(istream& in,person& a);
-        friend std::ostream& operator<<(std::ostream& out,const person a);
+        friend std::ostream& operator<<(std::ostream& out,const person& a);
+        virtual void menu()=0;
+};
+
+class guest: public person{
+    private:
+        // ? std::vector<item> cart;
+    public:
+        virtual void menu();
+        virtual void input();
+        virtual void output();
+        virtual void export();
+        virtual double payment();
+        virtual void print(std::ostream&) const;
+        virtual void load(std::istream&);
+        friend std::istream& operator>>(istream& in,guest& a);
+        friend std::ostream& operator<<(std::ostream& out,const guest& a);
+        // ? virtual bool buy(std::vector<item>,int);
+};
+
+class member: public guest{
+
 };
 
 #endif // !PERSON_H_

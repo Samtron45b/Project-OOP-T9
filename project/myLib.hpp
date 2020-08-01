fd599b0b35ba685b581887afbf2bc5215efa5078
Samtron45b <tien.nnang@gmail.com>
2020-08-02 02:19:07 +0700

    delete default func

--- project/myLib.hpp
+++ project/myLib.hpp
@@ -4,7 +4,7 @@
 #include <tuple>
 #include <string>
 
-std::tuple<int,int,int> currentTime(bool per12=true);
+std::tuple<int,int,int> currentTime(bool per12);
 int makeFolder(const std::string& s);
 bool makeDir(const std::string& dir);
 void clearConsole();
